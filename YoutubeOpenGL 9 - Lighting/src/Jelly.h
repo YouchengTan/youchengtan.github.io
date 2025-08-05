941edc426417fbb53bb057fb1bcbc096ebc58f85
Youcheng Tan <youcheng811@gmail.com>
2025-08-04 20:10:54 -0700

    put cpp and header files in src

---
+++ YoutubeOpenGL 9 - Lighting/src/Jelly.h
@@ -0,0 +1,42 @@
+#ifndef JELLY_H
+#define JELLY_H
+
+#include <glm/glm.hpp>
+#include <vector>
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+
+class Jelly {
+public:
+    Jelly(glm::vec3 center, float radius, glm::vec3 velocity, glm::vec3 acceleration, float pointMass, float springStrength, int springsPerEdge);
+
+    void Render();
+    void apply_idle_wobble(float time);
+    void apply_punch();
+    void resolve_ground_collision();
+
+
+private:
+    glm::vec3 center;
+    float radius;
+    glm::vec3 velocity;
+    glm::vec3 acceleration;
+    float pointMass;
+    float springStrength;
+    int springsPerEdge;
+
+    std::vector<GLfloat> vertices;
+    std::vector<GLuint> indices;
+
+    VAO vao;
+    VBO* vbo;
+    EBO* ebo;
+
+    void GenerateCubeMesh();
+    std::vector<GLfloat> originalVertices; // store the initial unmodified vertex positions
+
+};
+
+#endif
+
